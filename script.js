20e09ab0bf2e8f3aaf97cb71fdb96bd64f4a0ad7
jporter <javayporter@gmail.com>
2020-02-24 15:09:14 -0500

    updated charTypes variable in script.js file

--- script.js
+++ script.js
@@ -1,65 +1,53 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordLth = prompt("Please choose your password length. Enter a number between 8 and 128.");
-// var passwordLthVal = // if user enters an interger between 8-128 alert(ThankyouNext) else return an error and do not proceed to next prompt // 
+
+
 
 
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword() 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", function() {
-  passwordLth;
-}
-
-
-// document.querySelector('#bgnBtn').addEventListener('click', function() {
-//   var userAdjective = prompt("Please provide an Adjective");
-//   alert (userAdjective);
-// }
-
-//  var submitAnswer = function() {
-
-//   var radios = document.getElementsByName('choice');
-//   var val= "";
-//   for (var i = 0, length = radios.length; i < length; i++) {
-//       if (radios[i].checked) {
-//          val = radios[i].value; 
-//          break;
-//        }
-//   }
+generateBtn.addEventListener("click", promptUser) 
+  function promptUser() {
+    //prompts user to enter password lenghth
+  var passwordLth = prompt("Please choose your password length. Enter a number between 8 and 128.");
+  alert("Thank you. You entered " + passwordLth.value + ". Click OK to continue.")
   
-//   if (val == "" ) {
-//     alert('please select choice answer');
-//   } else if ( val == "Scripting" ) {
-//     alert('Answer is correct !');
-//   } else {
-//     alert('Answer is wrong');
-//   }
-// };
+    //prompts user to select character type(s)
+  var charTypes = prompt("Please select the character types you'd like used in your password. Enter 1 for lowercase. Enter 2 for uppercase. Enter 3 for numeric. Enter 4 for special characters. You may select multiple types. For example if you'd like to include lowercase, uppercase, and special characters in your password, you'd enter 124.");
+};
 
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
+
+  
+
+
+
+
+
+// ```
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+// ```
